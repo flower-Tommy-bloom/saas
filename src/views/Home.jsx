9b566cc4b813df8640b5b5faba46c70350a4bfda
tom <415157361@qq.com>
2019-10-31 22:05:43 +0800

    适配完成

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,17 +1,10 @@
 import React, {PureComponent} from 'react'
-import { Button } from 'react-weui'
-import '@/style/home1.less'
 export default class Home extends PureComponent{
   render(){
     return (
       <div >
         <div className="home">
-          Hello World!
-          <Button>hhh </Button>
-        </div>
-        <div className="home2">
-          Hello World!
-          <Button>hhh </Button>
+          <span>hello world</span>
         </div>
       </div>
     )
